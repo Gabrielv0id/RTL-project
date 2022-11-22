fb137810b4081c5624830ec36ea62ee9ac3ca4d6
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-11-22 17:51:58 -0300

    exercicio 3 feito

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente FavoritePokemon', () => {
+  test('A página deverá renderizar uma mensagem, caso a pessoa não tenha pokémon favoritos', () => {
+    const pokemonList = [];
+    render(<FavoritePokemon pokemonList={ pokemonList } />);
+
+    const textEl = screen.getByText(/no favorite pokémon found/i);
+
+    expect(textEl).toBeInTheDocument();
+  });
+
+  test('A página deverá renderizar os pokémon favoritados', () => {
+    const pokemonList = [{
+      id: 65,
+      name: 'Alakazam',
+      type: 'Psychic',
+      averageWeight: {
+        value: '48.0',
+        measurementUnit: 'kg',
+      },
+    }];
+    renderWithRouter(<FavoritePokemon pokemonList={ pokemonList } />);
+
+    const textEl = screen.getByText(/alakazam/i);
+
+    expect(textEl).toBeInTheDocument();
+  });
+});
