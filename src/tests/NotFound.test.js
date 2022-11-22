112ac2434fe8e08ce450f6f02e88b7393abb8c09
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-11-22 18:09:35 -0300

    exercicio 4 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Verifica a funcionalidade do componente NotFound', () => {
+  test('Verifica se a página contém um h2 com o texto "Page requested not found"', () => {
+    render(<NotFound />);
+
+    const h2El = screen.getByRole('heading', {
+      name: 'Page requested not found',
+      level: 2,
+    });
+
+    expect(h2El).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém a imagem correta', () => {
+    render(<NotFound />);
+
+    const imgEl = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    expect(imgEl.src).toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
